23434369caa0f836e03dd2063cc3642f4784bbcf
Ann Smith <89346341+rie1973@users.noreply.github.com>
2023-04-06 16:23:45 -0400

    Add files via upload

---
+++ bullet.py
@@ -0,0 +1,29 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    def __init__(self, ai_game): 
+        super().__init__()
+        
+        #bullet object at current position
+        self.screen =ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        #create bullet rect at (0, 0), set correct position
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, 
+                                self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
+        #store bullet position as decimal value
+        self.y = float(self.rect.y)
+
+    def update(self):
+        #update bullet's decimal position
+        self.y -= self.settings.bullet_speed
+
+        #update rect position
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
--- ship.py
+++ ship.py
@@ -8,6 +8,7 @@ class Ship:
     #initialize ship, set start position
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #load ship image, get rect
@@ -17,13 +18,22 @@ class Ship:
         #start new ship at center bottom of screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store decimal value for ship horizontal position
+        self.x = float(self.rect.x)
+
         #movement flag
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
-    
+        #update ship position based on movement flags
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #update rect oject from self.x
+        self.rect.x = self.x
 
     #draw shop at current location
     def blitme(self):
